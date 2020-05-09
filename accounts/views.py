a6ccfed611e8170000f69a100bb79ad26082e268
Sara Oliver <s.oliver1@icloud.com>
2020-05-09 22:32:32 +0000

    user profile

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from accounts.forms import UserLoginForm, UserRegistrationForm
 
 
 def index(request):
     """Return the index.html file"""
     return render(request,  'index.html')
-
 
 @login_required
 def logout(request):
@@ -26,7 +26,7 @@ def login(request):
 
         if login_form.is_valid():
             user = auth.authenticate(username=request.POST['username'],
-                                     password=request.POST['password'])
+                                    password=request.POST['password'])
             messages.success(request, "You have successfully logged in!")
 
             if user:
@@ -41,6 +41,29 @@ def login(request):
 
 def registration(request):
     """Render the registration page"""
-    registration_form = UserRegistrationForm()
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+
+    if request.method == "POST":
+        registration_form = UserRegistrationForm(request.POST)
+
+        if registration_form.is_valid():
+            registration_form.save()
+
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password1'])
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully registered")
+            else:
+                messages.error(request, "Unable to register your account at this time")
+    else:
+        registration_form = UserRegistrationForm()
     return render(request, 'registration.html', {
         "registration_form": registration_form})
+
+
+def user_profile(request):
+    """The user's profile page"""
+    user = User.objects.get(email=request.user.email)
+    return render(request, 'profile.html', {"profile": user})
