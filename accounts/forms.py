788b1d5631a328e0c608d72820e68cd9d4d716ab
Sara Oliver <s.oliver1@icloud.com>
2020-05-09 14:43:39 +0000

    user creation form setup

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 
 class UserLoginForm(forms.Form):
@@ -6,3 +9,16 @@ class UserLoginForm(forms.Form):
 
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(UserCreationForm):
+    """Form used to register a new user"""
+
+    password = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password Confirmation",
+        widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password1', 'password2']
